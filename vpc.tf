90793967da749c9fbfff3c1b462e49eb508ba342
shaik saidavali <shaiksaidavali223@gmail.com>
2026-02-02 16:04:21 +0530

    vpc-module

--- vpc.tf
+++ vpc.tf
@@ -152,16 +152,39 @@ resource "aws_nat_gateway" "nat" {
     depends_on = [ aws_internet_gateway.main ]
   }
   
+  # private egress route through NAT
   resource "aws_route" "private" {
     route_table_id = aws_route_table.private.id
     destination_cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   
   }
+    # database egress route through NAT
    resource "aws_route" "database" {
     route_table_id = aws_route_table.database.id
     destination_cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   
   }
+
+  resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = aws_subnet.public[count.index]
+    route_table_id = aws_route_table.public.id
+    
+  }
+  resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+    subnet_id = aws_subnet.private[count.index]
+    route_table_id = aws_route_table.private.id
+    
+  }
+  resource "aws_route_table_association" "database" {
+    count = length(var.database_subnet_cidrs)
+    subnet_id = aws_subnet.database[count.index]
+    route_table_id = aws_route_table.database.id
+    
+  }
+
+
 
